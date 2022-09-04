57a4edbfa575ecaf98cd24bab9266243e0312ec5
Changho Hwang <changhohwang@microsoft.com>
2022-09-04 02:39:07 +0000

    ARK support.

--- include/cutlass/epilogue/threadblock/epilogue.h
+++ include/cutlass/epilogue/threadblock/epilogue.h
@@ -55,6 +55,8 @@
 #include "cutlass/epilogue/threadblock/epilogue_base.h"
 #include "cutlass/epilogue/threadblock/predicated_tile_iterator.h"
 
+#include "sync.h"
+
 ////////////////////////////////////////////////////////////////////////////////
 
 namespace cutlass {
@@ -210,8 +212,8 @@ public:
       //
       // Convert and store fragment
       //
-      
-      __syncthreads();
+
+      ark::sync_warps<Base::WarpCount::kCount * 32>();
 
       typename AccumulatorFragmentIterator::Fragment accum_fragment;
 
@@ -220,7 +222,7 @@ public:
 
       this->warp_tile_iterator_.store(accum_fragment);
 
-      __syncthreads();
+      ark::sync_warps<Base::WarpCount::kCount * 32>();
 
       //
       // Load fragments from shared memory
